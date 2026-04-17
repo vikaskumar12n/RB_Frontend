96e65fc3279fa51ff48cbe0e8d99557ae55d3dc8
vikaskumar12n <vvikaskumar393@hgmail.com>
2026-04-17 16:19:06 +0530

    fix: add vercel proxy for API calls

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,9 +2,24 @@
 const API_URL = import.meta.env.VITE_API_URL;
 export const saveToAPI = async (resumeData, templateId) => {
   try {
-     console.log("API received data:", resumeData);
-        console.log("Template ID:", templateId);
-    const res = await fetch(`${API_URL}/api/resume/save`, {
+    console.log("API received data:", resumeData);
+    console.log("Template ID:", templateId);
+    // const res = await fetch(`${API_URL}/api/resume/save`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     ...resumeData,
+    //     templateId,
+    //   }),
+    // });
+    const url = '/api/resume/save';
+
+    console.log("🌐 Calling API URL:", url);
+    console.log("📦 Resume Data:", resumeData);
+    console.log("🏷️ Template ID:", templateId);
+    const res = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
